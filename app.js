ab237ad1fbf09f7bad39338211f8a44bce9341d9
faraja17 <farajakthompson@gmail.com>
2022-08-05 20:13:19 -0400

    changed client to mailchimp

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.post("/", function(req, res) {
 
     const run = async () => {
         try {    
-            const response = await client.lists.addListMembers("7f0aa8ef30", {
+            const response = await mailchimp.lists.addListMember("7f0aa8ef30", {
                     
                 email_address: subscribingUser.email,
                 status: "subscribed",
